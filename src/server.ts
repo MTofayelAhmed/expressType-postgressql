d68912308bd573a2deea16ab9e5d13babc829c3e
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-12-11 20:22:14 +0100

    follow modular pattern

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,16 @@
-import dotenv from "dotenv";
-import express, { NextFunction, Request, Response } from "express";
-import path from "path";
-import { Pool } from "pg";
 
-dotenv.config({ path: path.join(process.cwd(), "/src/.env") });
+import express, { NextFunction, Request, Response } from "express";
+
+
+import config from "./config";
+import initDB, { pool } from "./config/db";
+
+
 
 const app = express();
-const port = 5000;
+const port = config.port;
 
-const pool = new Pool({ connectionString: `${process.env.CONNECTION_STRING}` });
-
-const initDB = async () => {
-  await pool.query(
-    `CREATE TABLE IF NOT EXISTS users(
-        id  SERIAL PRIMARY KEY ,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(50) UNIQUE NOT NULL,
-        age INT,
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-
-
-        
-        )`
-  );
-
-  await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos (
-      id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES users(id) ON DELETE CASCADE,
-      title VARCHAR(220) NOT NULL,
-      description TEXT,
-      completed BOOLEAN DEFAULT false,
-      due_date DATE,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-
-      )  
-        `);
-};
-
+// initializing DB
 initDB();
 // parser
 app.use(express.json());
