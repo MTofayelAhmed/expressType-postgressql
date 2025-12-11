f2cd15e572ed55f25dbf364abfdef39791b1ad36
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-12-11 09:15:20 +0100

    done with fetch data from database

--- src/server.ts
+++ src/server.ts
@@ -81,7 +81,7 @@ app.get("/users", async (req: Request, res: Response) => {
     res.status(200).json({
       success: true,
       message: "Users fetched successfully",
-      data: result.rows,
+      data: result.rows
     });
   } catch (err: any) {
     res.status(500).json({
@@ -90,6 +90,33 @@ app.get("/users", async (req: Request, res: Response) => {
     });
   }
 });
+
+// get api for single users 
+
+app.get("/user/:id", async (req: Request, res: Response)=> {
+  try {
+const result = await pool.query(`SELECT * FROM users WHERE id= $1 `, [req.params.id])
+
+if(result.rows.length == 0){
+  res.status(404).json({
+    message: "data not found"
+  })
+}
+else {
+  res.status(200).json({
+    success: true,
+    message: "fetched single data successfully",
+    data: result.rows[0]
+  })
+}
+  }
+  catch(err: any){
+     res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
