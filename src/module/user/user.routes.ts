0a75c6b933d66936377448b575b11b9dd39bb722
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-12-14 14:11:21 +0100

    done with auth middleware

--- src/module/user/user.routes.ts
+++ src/module/user/user.routes.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 // routes => controller => service 
 
 const router = express.Router()
 
 router.post("/", userController.createUser )
 // for get Routes
-router.get("/", userController.getUser)
+router.get("/", auth(), userController.getUser)
 
 // get single users 
 
