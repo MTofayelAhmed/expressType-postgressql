46701044147bd5a9c62289e48062038837e4fd26
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-12-14 11:39:26 +0100

    jwt web token initiated

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { pool } from "../../config/db";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import  jwt from'jsonwebtoken';
+import { userController } from "../user/user.controller";
 const loginUser = async (email: string, password: string) => {
   const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [
     email,
@@ -14,4 +16,16 @@ const loginUser = async (email: string, password: string) => {
   if(!match){
     return false
   }
+const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
+  const token = jwt.sign({name: user.name, email: user.email}, secret ,{
+    expiresIn: "7d"
+  } )
+
+  return {token, user}
+
+  
+
 };
+export const authService = {
+  loginUser
+}
