882846a711c17568dbed583cb00dfdd34cb82102
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-12-14 15:26:25 +0100

    done with role setup in auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,15 +1,28 @@
 import { NextFunction, Request, Response } from "express"
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 import config from "../config";
-const auth = ()=> {
-    return  (req: Request, res: Response, next: NextFunction)   => {
+const auth = (...roles: string[])=> {
+    return async  (req: Request, res: Response, next: NextFunction)   => {
+
+try {
         const token = req.headers.authorization;
        if(!token){
         return res.status(500).json({message: "YOu are not allowed"})
        }
 
        const decoded = jwt.verify(token, config.jwtSecret as string )
+       req.user = decoded as JwtPayload
         next()
+    
+} catch (err: any) {
+   res.status(500).json({
+    success: false,
+    message: err.message
+   }) 
+}
+
+
+    
     }  
 }
 
